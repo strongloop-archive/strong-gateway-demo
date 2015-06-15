a6515c69d511c475a0b743e3804100a75cc5642d
Simon Ho <simon@strongloop.com>
2015-06-15 15:56:54 -0700

    Fix Windows copy-file scripts

--- notes-app-gateway/sample-configs/phase-4/copy-files.cmd
+++ notes-app-gateway/sample-configs/phase-4/copy-files.cmd
@@ -1,4 +1,4 @@
 xcopy server.js ..\..\server.js
-xcopy /s build-servers* ..\..\
-xcopy /s -r gateway-server\* ..\..\gateway-server\
-xcopy /s -r web-server\* ..\..\web-server\
+xcopy /s /y build-servers* ..\..\
+xcopy /s /y gateway-server\* ..\..\gateway-server\
+xcopy /s /y web-server\* ..\..\web-server\
--- notes-app-gateway/sample-configs/phase-6/copy-files.cmd
+++ notes-app-gateway/sample-configs/phase-6/copy-files.cmd
@@ -1,4 +1,4 @@
 xcopy server.js ..\..\server.js
-xcopy /s build-servers* ..\..\
-xcopy /s gateway-server\* ..\..\gateway-server\
-xcopy /s web-server\* ..\..\web-server\
+xcopy /s /y build-servers* ..\..\
+xcopy /s /y gateway-server\* ..\..\gateway-server\
+xcopy /s /y web-server\* ..\..\web-server\

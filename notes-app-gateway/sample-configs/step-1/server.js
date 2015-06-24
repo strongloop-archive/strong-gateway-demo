2bc15fc2f2dc583a2df8a1fd2e54d62114f31d09
Simon Ho <simon@strongloop.com>
2015-06-24 14:52:40 -0700

    Update step 1

--- notes-app-gateway/sample-configs/step-1/server.js
+++ notes-app-gateway/sample-configs/step-1/server.js
@@ -1,5 +1,13 @@
+var authServer = require('./auth-server');
 var client = require('./client');
 var resourceServer = require('./resource-server');
 
-client.start();
-resourceServer.start();
+authServer.set('port', 3002);
+authServer.set('https-port', 3202);
+authServer.set('url', 'https://localhost:3202');
+
+authServer.once('loaded', function() {
+  authServer.start();
+  client.start();
+  resourceServer.start();
+});

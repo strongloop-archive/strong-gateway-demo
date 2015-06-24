4c844c5aa2bf79d8b9eaf2d30bc558f5330d4702
Simon Ho <simon@strongloop.com>
2015-06-24 11:33:03 -0700

    Update notes-app-plain

--- notes-app-plain/server.js
+++ notes-app-plain/server.js
@@ -1,12 +1,5 @@
 var apiServer = require('./resource-server');
 var webServer = require('./client');
 
-apiServer.once('started', function() {
-  console.log('Notes API server ready at: %s', apiServer.get('url'));
-});
-webServer.once('started', function() {
-  console.log('Notes web server ready at: %s', webServer.get('url'));
-});
-
 apiServer.start();
 webServer.start()

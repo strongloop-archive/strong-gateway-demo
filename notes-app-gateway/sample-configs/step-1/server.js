f473a4f180c5adb2ca8a5d021f7d2341d411213c
Simon Ho <simon@strongloop.com>
2015-06-24 11:52:19 -0700

    Add step 1

--- notes-app-gateway/sample-configs/step-1/server.js
+++ notes-app-gateway/sample-configs/step-1/server.js
@@ -1,12 +1,5 @@
-var apiServer = require('./api-server');
-var webServer = require('./web-server');
+var client = require('./client');
+var resourceServer = require('./resource-server');
 
-apiServer.once('started', function() {
-  console.log('API server is ready - %s', apiServer.get('url'));
-});
-webServer.once('started', function() {
-  console.log('Web server is ready - %s', webServer.get('url'));
-});
-
-apiServer.start();
-webServer.start();
+client.start();
+resourceServer.start();

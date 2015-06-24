77524bcd1b24f731aa7a449323687e6e6312c3eb
Simon Ho <simon@strongloop.com>
2015-06-24 11:00:34 -0700

    Update step 0

--- notes-app-gateway/sample-configs/step-0/server.js
+++ notes-app-gateway/sample-configs/step-0/server.js
@@ -1,12 +1,5 @@
 var client = require('./client');
 var resourceServer = require('./resource-server');
 
-client.once('started', function() {
-  console.log('Client ready at %s', client.get('url'));
-});
-resourceServer.once('started', function() {
-  console.log('Resource server ready at %s', resourceServer.get('url'));
-});
-
 client.start();
 resourceServer.start();

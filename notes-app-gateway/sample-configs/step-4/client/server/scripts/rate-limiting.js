2e447fe841f1d15884c810096fc0e79c5af3ba4b
Simon Ho <simon@strongloop.com>
2015-06-24 17:18:12 -0700

    Update step 4

--- notes-app-gateway/sample-configs/step-4/client/server/scripts/rate-limiting.js
+++ notes-app-gateway/sample-configs/step-4/client/server/scripts/rate-limiting.js
@@ -9,38 +9,39 @@ var form = {
 };
 
 function printRateLimitHeaders(err, res) {
-  console.log('%s: Limit %d Remaining: %d Reset: %d',
+  console.log('Key: %s, Limit %d Remaining: %d Reset: %d',
     res.headers['x-ratelimit-key'] || null,
     res.headers['x-ratelimit-limit'] || null,
     res.headers['x-ratelimit-remaining'] || null,
     res.headers['x-ratelimit-reset'] || null);
 }
 
-var port = process.env.PORT || 3005;
-var count = Number(process.argv[2]) || 500;
+var authServerPort = process.env.PORT || 3202;
+var iterations = Number(process.argv[2]) || 500;
 
-// Request the access token
+// Request an access token
 request.post({
-  url: 'https://localhost:' + port + '/oauth/token',
+  url: 'https://localhost:' + authServerPort + '/oauth/token',
   headers: {
     'Content-Type': 'application/x-www-form-urlencoded',
     'Accept': 'application/json'
   },
-  // Use the client id/secret in the Authorization header
+  // Use the client id/secret in the authorization header
   auth: {
     user: '123',
     pass: 'secret'
   },
-  // Allow self-signed SSL certs
+  // Allow self-signed SSL certificates
   strictSSL: false,
   form: form
 }, function(err, res, body) {
   var token = JSON.parse(body).access_token;
   console.log('Access Token: %s', token);
 
-  // Request a protected resource in a loop
-  for (var i = 0; i < count; i++) {
-    request.get('https://localhost:' + port + '/api/notes?access_token='
-        + token, {strictSSL: false}, printRateLimitHeaders);
+  // Request a resource repeatedly
+  for (var i = 0; i < iterations; i++) {
+    request.get('https://localhost:' + authServerPort +
+      '/api/notes?access_token=' + token, {strictSSL: false},
+      printRateLimitHeaders);
   }
 });

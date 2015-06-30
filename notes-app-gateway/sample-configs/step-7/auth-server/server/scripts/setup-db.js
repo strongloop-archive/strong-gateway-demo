a0c2efcf667ba829a8e325349b5c1c002a2b7142
Simon Ho <simon@strongloop.com>
2015-06-30 11:32:24 -0700

    Update setup-db.js

--- notes-app-gateway/sample-configs/step-7/auth-server/server/scripts/setup-db.js
+++ notes-app-gateway/sample-configs/step-7/auth-server/server/scripts/setup-db.js
@@ -1,13 +1,14 @@
 // Set the env to prod to avoid sample-data population
-process.env.NODE_ENV = 'prod';
+process.env.NODE_ENV= 'prod';
 var app = require('../server');
 
 if (app.dataSources.db) {
   console.log('Auto-updating database: %j', app.dataSources.db.settings);
   app.dataSources.db.autoupdate(function(err) {
-    if (err)
-      return console.error('Error in setting database: %j', err);
-    console.log('Database setup successful');
+    if(err) {
+      console.error('Error in setting database: %j', err);
+    }
+    console.log('Database is set up');
     app.dataSources.db.disconnect();
   });
 }
